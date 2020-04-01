6122fd8bcafceb6600fdf2467e258cbb231cd70f
katerina0510 <ketrin_tim@bk.ru>
2020-04-01 15:27:41 +0300

    refactoring for review

--- test_product_page.py
+++ test_product_page.py
@@ -22,6 +22,7 @@ class TestUserAddToBasketFromProductPage():
 		page.open()
 		page.should_not_be_success_message()
 
+	@pytest.mark.need_review
 	def test_user_can_add_product_to_basket(self, browser):
 		link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
 		browser.implicitly_wait(5)
@@ -31,6 +32,7 @@ class TestUserAddToBasketFromProductPage():
 		page.solve_quiz_and_get_code()
 		page.shoold_be_choosen_product()
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link_part', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, link_part):
 	link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link_part}"
@@ -71,6 +73,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
 	page.open()
 	page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 	page = ProductPage(browser, link)
@@ -79,6 +82,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 	login_page = LoginPage(browser, browser.current_url)
 	login_page.should_be_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 	page = ProductPage(browser, link)
